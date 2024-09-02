f9c5505cfb71bd405790d631cbbf9d45a171e903
Adithya Behera <adityabeherda@gmail.com>
2024-09-02 16:48:52 +0530

    cors origin update

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://668fe4a7b7add600854acee6--edtech-academy.netlify.app",
+		origin:"https://edtech-academy.netlify.app",
 		credentials:true,
 	})
 )
